3e554144368790b52041f518ac0a2e02a188cf85
isti2623 <istiaqueahmed2019.bd@gmail.com>
2021-08-20 18:36:04 +0600

    update

--- js/script.js
+++ js/script.js
@@ -17,16 +17,25 @@ const shipping = document.getElementById("shipping");
 
 const total = document.getElementById("total");
 
+function updateTotal() {
+    const shippingCharge = Number(shipping.innerText);
+    const productPrice = Number(price.innerText);
+    const grandTotal = shippingCharge + productPrice;
+    total.innerText = grandTotal;
+
+}
 
 
 free.addEventListener('click', function () {
     shipping.innerText = 5;
-    let balance = total + 5;
-    total.innerText = balance;
+    updateTotal();
+
 })
 
 express.addEventListener('click', function () {
     shipping.innerText = 30;
+
+    updateTotal();
 })
 
 
@@ -38,7 +47,8 @@ first.addEventListener('click', function () {
     art.src = "images/1.jpg";
     desc.innerText = "First - is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
     price.innerText = 750;
-    total.innerText = 750;
+    updateTotal();
+
 
 })
 
@@ -46,14 +56,16 @@ second.addEventListener('click', function () {
     art.src = "images/2.jpg";
     desc.innerText = "Second - is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
     price.innerText = 550;
-    total.innerText = 550;
+    updateTotal();
+
 })
 
 third.addEventListener('click', function () {
     art.src = "images/3.jpg";
     desc.innerText = "Third - is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
     price.innerText = 300;
-    total.innerText = 300;
+    updateTotal();
+
 })
 
 
