fc9af50907befacd7cf6910cc85da19a7b37d80b
isti2623 <istiaqueahmed2019.bd@gmail.com>
2021-08-20 11:09:36 +0600

    new update

--- js/script.js
+++ js/script.js
@@ -8,10 +8,37 @@ const desc = document.getElementById("desc");
 
 const price = document.getElementById("price");
 
+
+const free = document.getElementById("free");
+const express = document.getElementById("express");
+
+
+const shipping = document.getElementById("shipping");
+
+const total = document.getElementById("total");
+
+
+
+free.addEventListener('click', function () {
+    shipping.innerText = 5;
+    let balance = total + 5;
+    total.innerText = balance;
+})
+
+express.addEventListener('click', function () {
+    shipping.innerText = 30;
+})
+
+
+
+
+
+
 first.addEventListener('click', function () {
     art.src = "images/1.jpg";
     desc.innerText = "First - is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
     price.innerText = 750;
+    total.innerText = 750;
 
 })
 
@@ -19,10 +46,14 @@ second.addEventListener('click', function () {
     art.src = "images/2.jpg";
     desc.innerText = "Second - is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
     price.innerText = 550;
+    total.innerText = 550;
 })
 
 third.addEventListener('click', function () {
     art.src = "images/3.jpg";
     desc.innerText = "Third - is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
     price.innerText = 300;
+    total.innerText = 300;
 })
+
+
